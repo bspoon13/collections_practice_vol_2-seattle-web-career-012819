dedbeb9c7aa267a9d6c0a9051d851c2619c73c93
Billy Witherspoon <wwither@gmail.com>
2019-01-25 09:11:11 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -92,7 +92,7 @@ organized_hash = {}
   school_hash.each do |school_name, location|
     #binding.pry
     location.each do |key, city|
-      binding.pry
+      #binding.pry
       if organized_hash[:city] == city
         organized_hash[:city] << school_name
       else
